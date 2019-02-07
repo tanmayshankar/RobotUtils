77aa824c7a4e68fca079529ee757f5cd28866472
tanmayshankar <tanmay.shankar@gmail.com>
2019-02-07 18:09:54 -0500

    Adding cleaned up version of baxter planning.

--- scripts/Baxter_Planning.py
+++ scripts/Baxter_Planning.py
@@ -43,7 +43,7 @@
 ## We also import `rospy`_ and some messages that we will use:
 ##
 
-import sys, copy, rospy, moveit_commander
+import sys, copy, rospy, moveit_commander, numpy as np
 import moveit_msgs.msg, geometry_msgs.msg
 from math import pi
 from IPython import embed
@@ -73,65 +73,40 @@ def all_close(goal, actual, tolerance):
 
 	return True
 
-class MoveGroupPythonIntefaceTutorial(object):
+class MoveGroupPythonInterface(object):
 	"""MoveGroupPythonIntefaceTutorial"""
 	def __init__(self):
-		super(MoveGroupPythonIntefaceTutorial, self).__init__()
+		super(MoveGroupPythonInterface, self).__init__()
 
-		## BEGIN_SUB_TUTORIAL setup
-		##
 		## First initialize `moveit_commander`_ and a `rospy`_ node:
 		moveit_commander.roscpp_initialize(sys.argv)
-		rospy.init_node('move_group_python_interface_tutorial',
-										anonymous=True)
+		rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
 
-		## Instantiate a `RobotCommander`_ object. This object is the outer-level interface to
-		## the robot:
+		# Instantiate a `RobotCommander`_ object. This object is the outer-level interface to the robot:
 		robot = moveit_commander.RobotCommander()
 
-		## Instantiate a `PlanningSceneInterface`_ object.  This object is an interface
-		## to the world surrounding the robot:
+		# Instantiate a `PlanningSceneInterface`_ object.  This object is an interface to the world surrounding the robot:
 		scene = moveit_commander.PlanningSceneInterface()
 
-		## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
-		## to one group of joints.  In this case the group is the joints in the Panda
-		## arm so we set ``group_name = panda_arm``. If you are using a different robot,
-		## you should change this value to the name of your robot arm planning group.
-		## This interface can be used to plan and execute motions on the Panda:
-		# group_name = "panda_arm"
+		# Instantiate a `MoveGroupCommander`_ object.  This object is an interface to one group of joints. 
 		group_name = "left_arm"
 		group = moveit_commander.MoveGroupCommander(group_name)
 
-		## We create a `DisplayTrajectory`_ publisher which is used later to publish
-		## trajectories for RViz to visualize:
-		display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
-																									 moveit_msgs.msg.DisplayTrajectory,
-																									 queue_size=20)
+		# We create a `DisplayTrajectory`_ publisher which is used later to publish trajectories for RViz to visualize:
+		display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=20)	
 
-		## END_SUB_TUTORIAL
-
-		## BEGIN_SUB_TUTORIAL basic_info
-		##
-		## Getting Basic Information
-		## ^^^^^^^^^^^^^^^^^^^^^^^^^
+		# ## Getting Basic Information
 		# We can get the name of the reference frame for this robot:
 		planning_frame = group.get_planning_frame()
-		print "============ Reference frame: %s" % planning_frame
 
 		# We can also print the name of the end-effector link for this group:
 		eef_link = group.get_end_effector_link()
-		print "============ End effector: %s" % eef_link
 
-		# We can get a list of all the groups in the robot:
-		group_names = robot.get_group_names()
-		print "============ Robot Groups:", robot.get_group_names()
+		# We can get a list of all the groups in the robot: 
+		group_names = robot.get_group_names()	
 
-		# Sometimes for debugging it is useful to print the entire state of the
-		# robot:
-		print "============ Printing robot state"
-		print robot.get_current_state()
-		print ""
-		## END_SUB_TUTORIAL
+		# Sometimes for debugging it is useful to print the entire state of the robot:	
+		# print robot.get_current_state()
 
 		# Misc variables
 		self.box_name = ''
@@ -150,37 +125,38 @@ class MoveGroupPythonIntefaceTutorial(object):
 
 		# The go command can be called with joint values, poses, or without any
 		# parameters if you have already set the pose or joint target for the group
-		plan = self.group.go(joint_goal, wait=True)
+		# plan = self.group.go(joint_goal, wait=True)
 
-		# Calling ``stop()`` ensures that there is no residual movement
+		plan = self.group.plan(joint_goal)
+		self.group.execute(plan, wait=True)		
 		self.group.stop()	
-		
-		# For testing:
+
 		current_joints = self.group.get_current_joint_values()
 		return all_close(joint_goal, current_joints, 0.01), plan
 
-	def go_to_pose_goal(self, pose_goal):
+	def go_to_pose_goal(self, pose_goal=None):
 		# Planning to a Pose Goal
 		
 		# We can plan a motion for this group to a desired pose for the end-effector:
-		pose_goal = geometry_msgs.msg.Pose()
-		pose_goal.orientation.w = 1.0
-		pose_goal.position.x = 0.4
-		pose_goal.position.y = 0.1
-		pose_goal.position.z = 0.4
+		if pose_goal==None:
+			pose_goal = geometry_msgs.msg.Pose()		
+			pose_goal.orientation.w = 1.0
+			pose_goal.position.x = 0.4
+			pose_goal.position.y = -0.1
+			pose_goal.position.z = 0.4
 		self.group.set_pose_target(pose_goal)
 
-		## Now, we call the planner to compute the plan and execute it.
-		plan = self.group.go(wait=True)
-		# Calling `stop()` ensures that there is no residual movement
+		# Here, we can run plan without specifying a  joint angle target, because we have already set a pose target above. 
+		plan = self.group.plan()
+		self.group.execute(plan, wait=True)
 		self.group.stop()
+	
 		# It is always good to clear your targets after planning with poses.
 		# Note: there is no equivalent function for clear_joint_value_targets()
 		self.group.clear_pose_targets()
 
-		# For testing:
 		current_pose = self.group.get_current_pose().pose
-		return all_close(pose_goal, current_pose, 0.01)
+		return all_close(pose_goal, current_pose, 0.01), plan
 
 	def plan_cartesian_path(self, scale=1):
 		## BEGIN_SUB_TUTORIAL plan_cartesian_path
@@ -218,8 +194,6 @@ class MoveGroupPythonIntefaceTutorial(object):
 	def display_trajectory(self, plan):
 		display_trajectory_publisher = self.display_trajectory_publisher
 
-		## BEGIN_SUB_TUTORIAL display_trajectory
-		##
 		## Displaying a Trajectory
 		## ^^^^^^^^^^^^^^^^^^^^^^^
 		## You can ask RViz to visualize a plan (aka trajectory) for you. But the
@@ -235,51 +209,38 @@ class MoveGroupPythonIntefaceTutorial(object):
 		# Publish
 		display_trajectory_publisher.publish(display_trajectory);
 
-		## END_SUB_TUTORIAL
+	def parse_plan(self, plan, dofs=7):
 
-	def execute_plan(self, plan):
-		# Copy class variables to local variables to make the web tutorials more clear.
-		# In practice, you should use the class variables directly unless you have a good
-		# reason not to.
-		group = self.group
+		traj_length = len(plan.joint_trajectory.points)
+		plan_array = np.zeros((traj_length, dofs))
+		# Parses plan into numpy array of len (T x DoF)
+		for t in range(traj_length):
+			plan_array[t] = plan.joint_trajectory.points[t].positions
 
-		## BEGIN_SUB_TUTORIAL execute_plan
-		##
-		## Executing a Plan
-		## ^^^^^^^^^^^^^^^^
-		## Use execute if you would like the robot to follow
-		## the plan that has already been computed:
-		group.execute(plan, wait=True)
-
-		## **Note:** The robot's current joint state must be within some tolerance of the
-		## first waypoint in the `RobotTrajectory`_ or ``execute()`` will fail
-		## END_SUB_TUTORIAL
+		return plan_array			
 
 def main():
 	try:
-		print "============ Press `Enter` to begin the tutorial by setting up the moveit_commander (press ctrl-d to exit) ..."
-		raw_input()
-		tutorial = MoveGroupPythonIntefaceTutorial()
+		movegroup = MoveGroupPythonInterface()
 
-		print "============ Press `Enter` to execute a movement using a joint state goal ..."
-		raw_input()
-		tutorial.go_to_joint_state()
+		# joint_goal = movegroup.group.get_current_joint_values()
+		# joint_goal[0] += 0.2
+		# joint_goal[2] += 0.2
+		# joint_goal[6] += 0.2
+		# success, plan = movegroup.go_to_joint_state(joint_goal)	
+		
+		success, plan = movegroup.go_to_pose_goal()
+		embed()
+		plan_array = movegroup.parse_plan(plan)
+		embed()
 
-		print "============ Press `Enter` to execute a movement using a pose goal ..."
-		raw_input()
-		tutorial.go_to_pose_goal()
+		# print "============ Press `Enter` to plan and display a Cartesian path ..."
+		# raw_input()
+		# cartesian_plan, fraction = movegroup.plan_cartesian_path(10)
 
-		print "============ Press `Enter` to plan and display a Cartesian path ..."
-		raw_input()
-		cartesian_plan, fraction = tutorial.plan_cartesian_path(10)
-
-		print "============ Press `Enter` to display a saved trajectory (this will replay the Cartesian path)  ..."
-		raw_input()
-		tutorial.display_trajectory(cartesian_plan)
-
-		print "============ Press `Enter` to execute a saved path ..."
-		raw_input()
-		tutorial.execute_plan(cartesian_plan)
+		# print "============ Press `Enter` to display a saved trajectory (this will replay the Cartesian path)  ..."
+		# raw_input()
+		# movegroup.display_trajectory(cartesian_plan)
 
 	except rospy.ROSInterruptException:
 		return
