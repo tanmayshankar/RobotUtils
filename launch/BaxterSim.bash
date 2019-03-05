8172275386b28e2b3d915999742a0560382dd354
tanmayshankar <tanmayshankar@gmail.com>
2019-03-05 11:53:24 -0800

    Update

--- launch/BaxterSim.bash
+++ launch/BaxterSim.bash
@@ -7,4 +7,4 @@ roslaunch RobotUtils publish_description.launch &
 rosrun baxter_interface joint_trajectory_action_server.py &
 xvfb-run-safe roslaunch RobotUtils baxter_camera.launch &
 sleep 10
-rosrun RobotUtils Baxter_Ambidextrous_Planning.py &
+# rosrun RobotUtils Baxter_Ambidextrous_Planning.py joint_states:=/robot/joint_states &
