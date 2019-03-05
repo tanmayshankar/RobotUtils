af09f2107a03d3d932584273917003c0945f8a97
tanmayshankar <tanmayshankar@gmail.com>
2019-03-05 11:44:51 -0800

    Update

--- launch/BaxterSim.bash
+++ launch/BaxterSim.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-source_baxter
-xvfb-run-safe roslaunch baxter_gazebo baxter_world.launch
+# source_baxter
+xvfb-run-safe roslaunch baxter_gazebo baxter_world.launch &
 sleep 10
-roslaunch RobotUtils publish_description.launch
-rosrun baxter_interface joint_trajectory_action_server.py
-xvfb-run-safe roslaunch RobotUtils baxter_camera.launch
+roslaunch RobotUtils publish_description.launch &
+rosrun baxter_interface joint_trajectory_action_server.py &
+xvfb-run-safe roslaunch RobotUtils baxter_camera.launch &
 sleep 10
-rosrun RobotUtils Baxter_Ambidextrous_Planning.py
+rosrun RobotUtils Baxter_Ambidextrous_Planning.py &
