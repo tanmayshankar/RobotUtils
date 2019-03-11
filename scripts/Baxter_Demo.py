e991f1c34f6c59a4c6353ce56a7df9ec2d591601
tanmayshankar <tanmayshankar@github.com>
2019-03-11 17:00:21 -0400

    Update

--- scripts/Baxter_Demo.py
+++ scripts/Baxter_Demo.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 from Baxter_Ambidextrous_Planning import *
+from Reset_Robot import RobotResetManager
 
 def main():	
 	try:
@@ -7,26 +8,29 @@ def main():
 		movegroup = MoveGroupPythonInterface()
 		image_retriever = ImageRetriever()		
 
+		reset_manager = RobotResetManager()
 
 		# Wait for the nodes to initialize and for images to be published before you try to retrieve images. 
 		time.sleep(5)
 		
-		# # Show frontal camera image. 
-		image = image_retriever.retrieve_image(2)
-		#plt.imshow(image)
-		#plt.show()
+		# # # Show frontal camera image. 
+		# image = image_retriever.retrieve_image(2)
+		# #plt.imshow(image)
+		# #plt.show()
 
-		movegroup.left_limb.move_to_neutral()
-		# Show left camera image.
-		image = image_retriever.retrieve_image(3)
-		#plt.imshow(image)	
-		#plt.show()
+		# movegroup.left_limb.move_to_neutral()
+		# # Show left camera image.
+		# image = image_retriever.retrieve_image(3)
+		# #plt.imshow(image)	
+		# #plt.show()
 
-		movegroup.right_limb.move_to_neutral()	
-		# Show right camera image.
-		image = image_retriever.retrieve_image(2)
-		#plt.imshow(image)	
-		#plt.show()
+		# movegroup.right_limb.move_to_neutral()	
+		# # Show right camera image.
+		# image = image_retriever.retrieve_image(2)
+		# #plt.imshow(image)	
+		# #plt.show()
+		
+		embed()
 
 	except rospy.ROSInterruptException:
 		return
