40676792eed3e1605ba649dcb5e4f214795d64c8
tanmayshankar <tanmayshankar@github.com>
2019-03-25 11:10:39 -0400

    Update

--- scripts/Reset_Robot.py
+++ scripts/Reset_Robot.py
@@ -46,21 +46,22 @@ class RobotResetManager():
 		self.switch_controllers_off_request.strictness = 2
 
 		# Simulation reset. 
+		self.reset_world_service = rospy.ServiceProxy("/gazebo/reset_world",Empty)
 		self.reset_sim_service = rospy.ServiceProxy("/gazebo/reset_simulation",Empty)
 
 		# Baxter PyKDL objects. 
 		self.baxter_right_kin_obj = baxter_kinematics('right')
 		self.baxter_left_kin_obj = baxter_kinematics('left')
 		# Order of joint names that joint limits are stored as. 
-		jn =  ["right_s0", "right_s1", "right_e0", "right_e1", "right_w0", "right_w1", "right_w2"]
+		# jn =  ["right_s0", "right_s1", "right_e0", "right_e1", "right_w0", "right_w1", "right_w2"]
 
 		self.movegroup = movegroup_interface
 		# Create dictionaries of joint limits.
-		self.joint_limit_lower_dict = self.movegroup.recreate_dictionary("right", self.baxter_right_kin_obj.joint_limits_lower, jn)
-		self.joint_limit_upper_dict = self.movegroup.recreate_dictionary("right", self.baxter_right_kin_obj.joint_limits_upper, jn)
+		self.joint_limit_lower_dict = self.movegroup.recreate_dictionary("right", self.baxter_right_kin_obj.joint_limits_lower, self.baxter_right_kin_obj._joint_names)
+		self.joint_limit_upper_dict = self.movegroup.recreate_dictionary("right", self.baxter_right_kin_obj.joint_limits_upper, self.baxter_right_kin_obj._joint_names)
 		# Create sorted values. 
-		self.lower_limits = np.array([value for (key, value) in sorted(self.joint_limit_lower_dict.items())])
-		self.upper_limits = np.array([value for (key, value) in sorted(self.joint_limit_upper_dict.items())])
+		self.lower_limits = np.array([value for (key, value) in sorted(self.joint_limit_lower_dict.items())]) - 0.0005*np.ones((7))
+		self.upper_limits = np.array([value for (key, value) in sorted(self.joint_limit_upper_dict.items())]) + 0.0005*np.ones((7))
 
 	def set_to_joint_pose(self, joint_positions, joint_names=None): 
 		# (0) Reset and Enable. 
@@ -109,7 +110,8 @@ class RobotResetManager():
 	def hard_reset(self):
 
 		# If we do something like cross joint limits of the baxter and need a hard reset of the simulation entirely. 
-		self.reset_sim_service()
+		self.reset_sim_service()	
+		self.reset_world_service()	
 		self.movegroup.reset_and_enable()
 		
 	def check_and_reset(self):
