428784d2e9c33e4213268d1849596af449499ad0
tanmayshankar <tanmay.shankar@gmail.com>
2019-03-26 15:38:49 -0400

    Update

--- scripts/Reset_Robot.py
+++ scripts/Reset_Robot.py
@@ -21,30 +21,6 @@ class RobotResetManager():
 		# Create object to call services to start and stop controllers. 
 		self.switch_controller_service = rospy.ServiceProxy("/robot/controller_manager/switch_controller",SwitchController)
 
-		# Create service requests for the set config and switch controller services. 
-		self.config_request = SetModelConfigurationRequest()
-		self.config_request.model_name = "baxter"
-		self.config_request.urdf_param_name = "robot_description"
-		# Set default joint names and joint positions. 
-		# Can also set things for left arm by just providing corresponding dictionary. 
-		self.config_request.joint_names = ["right_s0","right_s1","right_e0","right_e1","right_w0","right_w1","right_w2"]
-		self.config_request.joint_positions = [0.,0.,0.,0.,0.,0.,0.]
-
-		# Creating service request to switch controllers on and off separately. 
-		# Request to switch on controllers. 
-		self.switch_controllers_on_request = SwitchControllerRequest()
-		# self.switch_controllers_on_request.start_controllers = ['joint_state_controller','left_joint_position_controller','right_joint_position_controller']
-		self.switch_controllers_on_request.start_controllers = ['joint_state_controller']
-		self.switch_controllers_on_request.stop_controllers = []
-		self.switch_controllers_on_request.strictness = 2
-		
-		# Request to switch off controllers. 
-		self.switch_controllers_off_request = SwitchControllerRequest()
-		self.switch_controllers_off_request.start_controllers = []
-		# self.switch_controllers_off_request.stop_controllers = ['joint_state_controller','left_joint_position_controller','right_joint_position_controller']
-		self.switch_controllers_off_request.stop_controllers = ['joint_state_controller']
-		self.switch_controllers_off_request.strictness = 2
-
 		# Simulation reset. 
 		self.reset_world_service = rospy.ServiceProxy("/gazebo/reset_world",Empty)
 		self.reset_sim_service = rospy.ServiceProxy("/gazebo/reset_simulation",Empty)
@@ -63,6 +39,43 @@ class RobotResetManager():
 		self.lower_limits = np.array([value for (key, value) in sorted(self.joint_limit_lower_dict.items())]) - 0.0005*np.ones((7))
 		self.upper_limits = np.array([value for (key, value) in sorted(self.joint_limit_upper_dict.items())]) + 0.0005*np.ones((7))
 
+	def setup(self):
+		self.set_config_request()
+		self.set_controller_on_request()
+		self.set_controller_off_request()
+
+	def set_config_request(self, joint_names=None, joint_positions=None):
+		# Create service requests for the set config and switch controller services. 
+		self.config_request = SetModelConfigurationRequest()
+		self.config_request.model_name = "baxter"
+		self.config_request.urdf_param_name = "robot_description"
+		# Set default joint names and joint positions. 
+		# Can also set things for left arm by just providing corresponding dictionary. 
+		if (joint_names is None) and (joint_positions is None):
+			self.config_request.joint_names = ["right_s0","right_s1","right_e0","right_e1","right_w0","right_w1","right_w2"]
+			self.config_request.joint_positions = [0.,0.,0.,0.,0.,0.,0.]
+		if joint_names:
+			self.config_request.joint_names = joint_names
+		if joint_positions:
+			self.config_request.joint_positions = joint_positions
+
+	def set_controller_on_request(self):
+		# Creating service request to switch controllers on and off separately. 
+		# Request to switch on controllers. 
+		self.switch_controllers_on_request = SwitchControllerRequest()
+		# self.switch_controllers_on_request.start_controllers = ['joint_state_controller','left_joint_position_controller','right_joint_position_controller']
+		self.switch_controllers_on_request.start_controllers = ['joint_state_controller']
+		self.switch_controllers_on_request.stop_controllers = []
+		self.switch_controllers_on_request.strictness = 2
+		
+	def set_controller_off_request(self):
+		# Request to switch off controllers. 
+		self.switch_controllers_off_request = SwitchControllerRequest()
+		self.switch_controllers_off_request.start_controllers = []
+		# self.switch_controllers_off_request.stop_controllers = ['joint_state_controller','left_joint_position_controller','right_joint_position_controller']
+		self.switch_controllers_off_request.stop_controllers = ['joint_state_controller']
+		self.switch_controllers_off_request.strictness = 2
+
 	def set_to_joint_pose(self, joint_positions, joint_names=None): 
 		# (0) Reset and Enable. 
 		self.movegroup.reset_and_enable()
@@ -74,9 +87,7 @@ class RobotResetManager():
 		self.pause_physics()
 		
 		# (3) Set Model Configuration. 
-		if joint_names:
-			self.config_request.joint_names = joint_names		
-		self.config_request.joint_positions = joint_positions
+		self.set_config_request(joint_names, joint_positions)
 		self.set_config_service(self.config_request)
 		self.set_config_service(self.config_request)
 
@@ -110,7 +121,7 @@ class RobotResetManager():
 	def hard_reset(self):
 
 		# If we do something like cross joint limits of the baxter and need a hard reset of the simulation entirely. 
-		self.reset_sim_service()	
+		# self.reset_sim_service()	
 		self.reset_world_service()	
 		self.movegroup.reset_and_enable()
 		
